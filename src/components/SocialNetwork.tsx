556a0b38c1d1527b8b021f5e3599ea8cba800cd8
bounis67 <boehmcorentin52@gmail.com>
2024-06-29 02:30:57 +0200

    feat: Add Tooltip to SocialNetwork buttons

--- src/components/SocialNetwork.tsx
+++ src/components/SocialNetwork.tsx
@@ -1,17 +1,19 @@
-import { Button } from "antd";
+import { Button, Tooltip } from "antd";
 import { socialNetworks } from "../data/socialNetworks";
 
 export const SocialNetwork = () => {
     return (
         <div className="flex flex-row justify-around gap-3">
             {socialNetworks.map((socialNetwork) => (
-                <Button
-                    type="default"
-                    icon={socialNetwork.icon}
-                    size={"large"}
-                    href={socialNetwork.url}
-                    target="_blank"
-                />
+                <Tooltip title={socialNetwork.name} key={socialNetwork.name}>
+                    <Button
+                        type="default"
+                        icon={socialNetwork.icon}
+                        size={"large"}
+                        href={socialNetwork.url}
+                        target="_blank"
+                    />
+                </Tooltip>
             ))}
         </div>
     );
